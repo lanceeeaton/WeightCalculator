8d750cf7c925ab643887824e7f54bcc000770d2e
lanceeeaton <27367647+lanceeeaton@users.noreply.github.com>
2026-03-09 16:03:40 -0400

    Init commit

---
+++ WeightCalculator/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WeightCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WeightCalculator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WeightCalculator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
